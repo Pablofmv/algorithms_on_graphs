f3c2934ffca262daeecd7284ad5991cf68b9b05f
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-09-11 09:33:51 -0400

    Added files to DS

---
+++ exploring_graphs4.py
@@ -0,0 +1,19 @@
+graphs = {
+    "A" : ["B"],
+    "B" : ["A"],
+    "C" : ["D"],
+    "D" : ["C","E"],
+    "E" : ["D"]
+}
+
+visited = {}
+
+def explore(v):
+    visited[v] = True
+    print("Visited:", v)
+    for nbr in graphs[v]:
+        if not visited.get(nbr, False):
+            explore(nbr)
+
+explore("C")
+
