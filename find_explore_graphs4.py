91c0246a096daefe922e7e9e8bb17d5959bedf0b
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-09-12 10:05:34 -0400

    Added files to DS

---
+++ find_explore_graphs4.py
@@ -0,0 +1,28 @@
+graph = {
+    "A": ["B", "C"],
+    "B": ["A", "D"],
+    "C": ["A"],
+    "D": ["B"],
+    "E": ["F"],
+    "F": ["E"]
+}
+
+components = []
+visited = {}
+
+def explore(v, comp):
+    visited[v] = True
+    comp.append(v)
+    for nbr in graph[v]:
+        if not visited.get(nbr, False):
+            explore(nbr, comp)
+
+def find_components():
+    for v in graph:
+        if not visited.get(v, False):
+            comp = []
+            explore(v, comp)
+            components.append(comp)
+
+find_components()
+print("Connected components", components)
