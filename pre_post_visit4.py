60ecb12a2e591deccdec1b846e005d49196923ad
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-09-17 19:13:32 -0400

    Added files to DS

---
+++ pre_post_visit4.py
@@ -0,0 +1,40 @@
+graph = {
+    "A": ["B", "C"],
+    "B": [],
+    "C": []
+}
+
+visited = {}
+pre = {}
+post = {}
+clock = 1
+
+def previsit(v):
+    global clock
+    pre[v] = clock
+    print(f"previsit({v}):{clock}")
+    clock += 1
+
+def postvisit(v):
+    global clock
+    post[v] = clock
+    print(f"postvisit({v}): {clock}")
+    clock += 1
+
+def explore(v):
+    visited[v] = True
+    previsit(v)
+    for w in graph[v]:
+        if not visited.get(w, False):
+            explore(w)
+    postvisit(v)
+
+def dfs():
+    for v in graph:
+        if not visited.get(v, False):
+            explore(v)
+
+dfs()
+
+print(f"Final Pre Numbers:", pre)
+print(f"Final Post Numbers:", post)
