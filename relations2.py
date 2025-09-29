5b150338cd6af23f40ffa1f5464d0ebd10621894
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-09-29 15:02:15 -0400

    Added files to DS

---
+++ relations2.py
@@ -0,0 +1,54 @@
+graph = {
+    'u': ['v'],
+    'v': ['w'],
+    'w': ['u'],  # back edge (w -> u creates a cycle)
+    'x': ['y'],
+    'y': []
+}
+
+visited = {}
+pre = {}
+post = {}
+clock = 1
+
+def previsit(v):
+    global clock
+    pre[v] = clock
+    print(f"previsit({v}) = {clock}")
+    clock += 1
+
+def postvisit(v):
+    global clock
+    post[v] = clock
+    print(f"postvisit({v} = {clock})")
+    clock += 1
+
+def explore(v):
+    visited[v] = True
+    previsit(v)
+    for nbr in graph[v]:
+        if not visited.get(nbr, False):
+            explore(nbr)
+    postvisit(v)
+
+for v in graph:
+    if not visited.get(v, False):
+        explore(v)
+
+print("Pre Time:", pre)
+print("Post Time:", post)
+
+def relation(u, v):
+
+    if pre[u] < pre[v] and post[v] < post[u]:
+        return f"{v} is nested inside {u}"
+    elif post[u] < pre[v] or post[v] < pre[u]:
+        return f"{u} and {v} are disjoint."
+    elif pre[v] < pre[u] and post[u] < post[v]:
+        return f"Back edge detected: {u} -> {v}"
+    else:
+        return "Other case."
+
+print(relation("u","v"))
+print(relation("u","x"))
+print(relation("w","u"))
