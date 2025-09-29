97927d4b6f0c0c3f6818f6eabf4f8b704b0728d9
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-09-29 15:27:55 -0400

    Added files to DS

--- relations3.py
+++ relations3.py
@@ -38,5 +38,17 @@ for v in graph:
 print("Pre times:", pre)
 print("Post time:", post)
 
+def relation(u, v):
+    if pre[u] < pre[v] and post[v] < post[u]:
+        return f"{v} is nested inside {u}"
+    elif post[u] < pre[v] or post[v] < pre[u]:
+        return f"{v} and {u} are disjoint." 
+    elif pre[v] < pre[u] and post[u] < post[v]:
+        return f"Back edge detected: {u} -> {v}"
+    else:
+        return "Other case"
 
+print(relation('u', 'v'))
+print(relation('u', 'x'))
+print(relation('w', 'u'))
 
