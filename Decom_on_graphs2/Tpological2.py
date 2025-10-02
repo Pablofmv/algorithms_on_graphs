f7abd36e1f2ed65fbde9b333d23d9e3a629c583f
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-10-02 09:55:37 -0400

    Added files to DS

---
+++ Decom_on_graphs2/Tpological2.py
@@ -0,0 +1,30 @@
+graph = {
+    5: [2, 0],
+    4: [0, 1],
+    2: [3],
+    3: [1],
+    0: [],
+    1: []
+}
+
+def topological_sort(graph):
+
+    visited = {}
+    order = []
+
+    def dfs(node):
+
+        if node not in visited:
+            visited[node] = True
+            for neighbor in graph[node]:
+                dfs(neighbor)
+            order.append(node)
+    
+    for node in graph:
+        dfs(node)
+    
+    return order[::-1]
+
+print("Topological Sort:", topological_sort(graph))
+
+            
