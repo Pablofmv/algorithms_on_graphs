e9101f1b293822373625880bdf5dcd247b8312c9
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-10-08 14:04:56 -0400

    Added files to DS

---
+++ Decom_on_graphs2/scc5.py
@@ -0,0 +1,75 @@
+
+def strongly_connected_components(graph):
+
+    #S1 - Reverse the graph:
+
+    reverse_graph = {}
+    for node in graph:
+        for neighbor in graph[node]:
+            if neighbor not in reverse_graph:
+                reverse_graph[neighbor] = []
+            reverse_graph[neighbor].append(node)
+        if node not in reverse_graph:
+            reverse_graph[node] = []
+    
+    #S2 - Run DFS in reverse graph to get postorder numbers:
+
+    postorder = {}
+    visited_reverse = {}
+    time = 1
+
+    def dfs_reverse(node):
+        nonlocal time
+        visited_reverse[node] = True
+        for neighbor in reverse_graph[node]:
+            if neighbor not in visited_reverse:
+                dfs_reverse(neighbor)
+        
+        postorder[node] = time
+        time += 1
+    
+    for node in reverse_graph:
+        if node not in visited_reverse:
+            dfs_reverse(node)
+    
+    #S3 - Run DFS in graph in reverse postorder number:
+
+    visited = {}
+    sccs = []
+
+    def dfs_original(node, component):
+        visited[node] = True
+        component.append(node)
+
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                dfs_original(neighbor, component)
+    
+    for v in sorted(postorder, key = postorder.get, reverse = True):
+        if v not in visited:
+            component = []
+            dfs_original(v, component)
+            sccs.append(component)
+    
+    return sccs
+
+graph = {
+    'A': ['B'],
+    'B': ['C'],
+    'C': ['A'],   # Cycle 1: A-B-C
+    'D': ['E'],
+    'E': ['F'],
+    'F': ['D']    # Cycle 2: D-E-F
+}
+
+print("Strongly Connected Components:")
+print(strongly_connected_components(graph))
+
+
+        
+    
+
+    
+
+
+
