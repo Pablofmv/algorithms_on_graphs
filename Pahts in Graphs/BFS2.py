37c314e0c2e6f9aab540c2a232fc1631ab851948
Pablo Mendoza <pablo_fmv@hotmail.com>
2025-10-12 12:20:59 -0400

    Added files to DS

---
+++ Pahts in Graphs/BFS2.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+def bfs(graph, start):
+
+    dist = {}
+
+    for node in graph:
+        dist[node] = float('inf')
+    dist[start] = 0
+
+    q = deque([start])
+
+    while q:
+        node = q.popleft()
+
+        for v in graph[node]:
+            if dist[v] == float("inf"):
+                q.append(v)
+                dist[v] = dist[node] + 1
+    
+    return dist
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+# Run BFS from node 'A'
+distances = bfs(graph, 'A')
+
+for node, d in distances.items():
+    print(f"Distance from A to {node}: {d}")
